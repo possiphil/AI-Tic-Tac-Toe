af984cfa4481fb3ffa2f23805af4673cd9266e55
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-22 16:58:32 +0100

    Initial Commit

---
+++ AI Tic Tac Toe/AI_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Tic_Tac_ToeApp.swift
+//  AI Tic Tac Toe
+//
+//  Created by Philipp Sanktjohanser on 22.12.22.
+//
+
+import SwiftUI
+
+@main
+struct AI_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
